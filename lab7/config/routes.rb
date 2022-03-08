b9cc8a88a20d0e921f20c20f865925c83dcb5f7d
YUPAiNkun <87040160+YUPAiNkun@users.noreply.github.com>
2022-03-08 15:13:47 +0300

    Delete lab7 directory

--- lab7/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root 'reports#index'
-
-  resources :reports, only: [:index, :new, :create, :edit, :update, :destroy]
-
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-end
