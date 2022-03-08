fa889ba005a25b19a1266844c090785c03cc66de
YUPAiNkun <87040160+YUPAiNkun@users.noreply.github.com>
2022-03-08 15:18:24 +0300

    Delete lab6 directory

--- lab6/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root 'reports#index'
-
-  resources :reports, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :users, only: [:new, :create]
-end
